fcfbe51cc83a6354f0d50a294b5ca3086b989508
Tarantula <169634892+tarantula212@users.noreply.github.com>
2024-09-27 15:36:59 +0530

    feat(spotify) - handle case where album name is present in title like `- From "album_name"`

--- plex-playlist-sync/utils/spotify.py
+++ plex-playlist-sync/utils/spotify.py
@@ -42,7 +42,7 @@ def _get_sp_user_playlists(
     return playlists
 
 def _cleanup_title(title: str) -> str:
-    title_match = re.search(r'^(.*?) \(From', title)
+    title_match = re.search(r'^(.*?) \(From| - From| \(from the Netflix Film', title)  # Updated regex to handle both cases
     return title_match.group(1).strip() if title_match else title
 
 def _cleanup_album_name(album: str) -> str:
